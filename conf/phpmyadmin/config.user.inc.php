73eca9b7b238ae996bd8246c7a180d9e727a4192
Juan Rodrigo <juanrodr00@student.kau.se>
2024-08-17 08:58:13 +0200

    added basic config

---
+++ conf/phpmyadmin/config.user.inc.php
@@ -0,0 +1,6 @@
+// <?php
+// if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+//     $_SERVER['HTTPS'] = 'on';
+// }
+// $cfg['PmaAbsoluteUri'] = 'https://localhost:443/';
+// ?>
